ccadf380f4170f5535a0eb8e5aeb9cd578f4a619
Zuhair Amirali <zuhairamirali7@gmail.com>
2025-10-05 00:02:24 +0500

    Start syscall lab

--- user/user.h
+++ user/user.h
@@ -39,7 +39,7 @@ int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
 char* sbrk(int);
 char* sbrklazy(int);
-
+int interpose(uint64 mask, const char *path);
 // printf.c
 void fprintf(int, const char*, ...) __attribute__ ((format (printf, 2, 3)));
 void printf(const char*, ...) __attribute__ ((format (printf, 1, 2)));
